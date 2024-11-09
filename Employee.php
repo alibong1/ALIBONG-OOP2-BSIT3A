60fffba7d218c70913f360a07a1dd0d462d1c894
alibong1 <129124895+alibong1@users.noreply.github.com>
2024-11-09 21:23:55 +0800

    Update Employee.php

--- Employee.php
+++ Employee.php
@@ -1,31 +1,26 @@
- <?php
-    require_once 'Person.php';
+<?php
 
-    abstract class Employee extends Person {
-        private $employeeId;    
-        private $companyName;
-        
+abstract class Employee {
+    protected string $name;
+    protected string $address;
+    protected int $age;
+    protected string $companyName;
+    
 
-        public function __construct($name, $address, $age, $companyName) {
-            parent::__construct($name, $address, $age);
-            $this->companyName = $companyName;
-        }
-
-        public function getEmployeeId() {
-            return $this->employeeId;
-        }
-
-        public function setEmployeeId($employeeId) {
-            $this->employeeId = $employeeId;
-        }
-        public function getCompanyName() {
-            return $this->companyName;
-        }
-
-        abstract public function earnings();
-
-        public function toString() {
-            return "Employee ID: $this->employeeId\n" . parent::toString() . "\nCompany: $this->companyName";
-        }
+    public function __construct(string $name, string $address, int $age, string $companyName) {
+        $this->name = $name;
+        $this->address = $address;
+        $this->age = $age;
+        $this->companyName = $companyName;
     }
-    ?>
+
+
+
+    abstract public function calculatePay(): float;
+
+    public function getDetails(): string {
+        return "\nName     : {$this->name}\nAddress  : {$this->address}\nAge      : {$this->age}\nCompany  : {$this->companyName}";
+    }
+}
+
+?>
