b8943895b5be624dc8add9657e53ac70e61cde09
alibong1 <129124895+alibong1@users.noreply.github.com>
2024-11-09 20:16:36 +0800

    Update Employee.php

--- Employee.php
+++ Employee.php
@@ -1,17 +1,31 @@
-<?php
-require_once 'Person.php';
+ <?php
+    require_once 'Person.php';
 
-abstract class Employee extends Person {
-    private string $companyName;
+    abstract class Employee extends Person {
+        private $employeeId;    
+        private $companyName;
+        
 
-    public function __construct($name, $address, $age, $companyName) {
-        parent::__construct($name, $address, $age);
-        $this->companyName = $companyName;
+        public function __construct($name, $address, $age, $companyName) {
+            parent::__construct($name, $address, $age);
+            $this->companyName = $companyName;
+        }
+
+        public function getEmployeeId() {
+            return $this->employeeId;
+        }
+
+        public function setEmployeeId($employeeId) {
+            $this->employeeId = $employeeId;
+        }
+        public function getCompanyName() {
+            return $this->companyName;
+        }
+
+        abstract public function earnings();
+
+        public function toString() {
+            return "Employee ID: $this->employeeId\n" . parent::toString() . "\nCompany: $this->companyName";
+        }
     }
-
-    abstract public function earnings();
-
-    public function __toString(): string {
-        return parent::__toString() . ", Company: {$this->companyName}";
-    }
-}
+    ?>
